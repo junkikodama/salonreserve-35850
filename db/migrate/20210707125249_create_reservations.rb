6858cd4c53cb0fb04710100a7ac9d615c775c72f
junkikodama <84299468+junkikodama@users.noreply.github.com>
2021-07-08 11:10:42 +0900

    activestorageインストール

--- db/migrate/20210707125249_create_reservations.rb
+++ db/migrate/20210707125249_create_reservations.rb
@@ -1,7 +1,12 @@
 class CreateReservations < ActiveRecord::Migration[6.1]
   def change
     create_table :reservations do |t|
-
+      t.date :day,            null: false
+      t.string :time,         null: false
+      t.datetime :start_time, null: false
+      t.integer :menu_id,     null: false
+      t.text :order,          null: false
+      t.references :user,     foreign_key: true
       t.timestamps
     end
   end
