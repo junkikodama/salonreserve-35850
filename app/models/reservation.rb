d4f5b3547db267f0fce9f912f7c5811ccc3ced7c
junkikodama <84299468+junkikodama@users.noreply.github.com>
2021-07-08 12:14:49 +0900

    現在時刻読み取り

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -12,27 +12,9 @@ class Reservation < ApplicationRecord
     validates :menu_id
   end
 
-  validate :date_before_start
-  validate :date_current_today
-  validate :date_three_month_end
-
-  def date_before_start
-    errors.add(:day, "は過去の日付は選択できません。予約画面から正しい日付を選択してください。") if day < Date.current
-  end
-
-  def date_current_today
-    errors.add(:day, "は当日は選択できません。予約画面から正しい日付を選択してください。") if day < (Date.current + 1)
-  end
-
-  def date_three_month_end
-    errors.add(:day, "は3ヶ月以降の日付は選択できません。予約画面から正しい日付を選択してください。") if (Date.current >> 3) < day
-  end
-
   def self.check_reservation_day(day)
     if day < Date.current
       return "過去の日付は選択できません。正しい日付を選択してください。"
-    elsif day < (Date.current + 1)
-      return "当日は選択できません。正しい日付を選択してください。"
     elsif (Date.current >> 3) < day
       return "3ヶ月以降の日付は選択できません。正しい日付を選択してください。"
     end
