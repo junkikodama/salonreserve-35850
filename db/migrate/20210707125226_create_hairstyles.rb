c95bf96f0b71396ca7c89eaa3ba42639efdb2a02
junkikodama <84299468+junkikodama@users.noreply.github.com>
2021-07-08 11:48:28 +0900

    エラーメッセージ追加

--- db/migrate/20210707125226_create_hairstyles.rb
+++ db/migrate/20210707125226_create_hairstyles.rb
@@ -1,6 +1,14 @@
 class CreateHairstyles < ActiveRecord::Migration[6.1]
   def change
     create_table :hairstyles do |t|
+      t.string :hairstylename,  null: false
+      t.text :detail,           null: false
+
+      t.integer :gender_id,     null: false
+      t.integer :hairlength_id, null: false
+      t.integer :haircolor_id,  null: false
+
+      t.references :user,       foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -56,8 +56,15 @@ ActiveRecord::Schema.define(version: 2021_07_08_020900) do
   end
 
   create_table "hairstyles", charset: "utf8", force: :cascade do |t|
+    t.string "hairstylename", null: false
+    t.text "detail", null: false
+    t.integer "gender_id", null: false
+    t.integer "hairlength_id", null: false
+    t.integer "haircolor_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_hairstyles_on_user_id"
   end
 
   create_table "menus", charset: "utf8", force: :cascade do |t|
@@ -91,5 +98,6 @@ ActiveRecord::Schema.define(version: 2021_07_08_020900) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "hairstyles", "users"
   add_foreign_key "reservations", "users"
 end
